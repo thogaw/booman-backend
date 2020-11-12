cdf03e71fd9b5dc710aa475cbbb48f8ef6cf95fc
Thorsten Gawantka <t.gawantka@gmail.com>
2020-11-12 23:30:19 +0100

    Create bookmark-component and configure testcontainers.

---
+++ src/main/resources/db/migration/V0_0_1_001__create_bookmark_entity.sql
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020 Thorsten Gawantka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+CREATE DOMAIN url AS text check (VALUE ~
+                                 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#()?&//=]*)');
+COMMENT ON DOMAIN url IS 'match URLs (http or https)';
+
+CREATE TABLE "bookmark"
+(
+    "id"   BIGSERIAL,
+    "name" VARCHAR(1023) NOT NULL,
+    "url"  URL           NOT NULL,
+
+    CONSTRAINT "pk_bookmark_id" PRIMARY KEY ("id")
+);
